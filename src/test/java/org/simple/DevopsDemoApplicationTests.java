fa64f48bdc8315f9da0c994d21a79cc55c26b618
alpha-svo <fsprotection16@hotmail.com>
2022-10-18 10:45:21 +0800

    chang springboot version

---
+++ src/test/java/org/simple/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
