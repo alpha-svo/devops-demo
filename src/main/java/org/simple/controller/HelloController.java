060175d5c93d935f0ff711c4ddfee4396d36461a
alpha-svo <fsprotection16@hotmail.com>
2022-11-02 08:45:20 +0800

    第一次 部署演示

--- src/main/java/org/simple/controller/HelloController.java
+++ src/main/java/org/simple/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping
     public String hello(){
-        return "Hello World! 第6次修改:移除了docker.sock访问权限修改，将在服务器启动时自动修改，不放置到jenkins中，添加了webhook,修复了pipeline语法问题";
+        return "Hello World! 第1次部署";
     }
 }
